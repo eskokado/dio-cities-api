8db9e691f4324b682a8843caf29fa842223f2933
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-21 17:49:57 -0300

    Initial commit

---
+++ src/main/java/com/github/eskokado/eskcitiesapi/EskCitiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.eskokado.eskcitiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskCitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskCitiesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/eskokado/eskcitiesapi/EskCitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eskokado.eskcitiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskCitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
