992c5d05b7c385a4323737fe62a9ee2acc55bcc5
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-22 05:50:26 -0300

    Perfecting resource Country with Pageable

--- src/main/java/com/github/eskokado/eskcitiesapi/countries/resources/CountryResource.java
+++ src/main/java/com/github/eskokado/eskcitiesapi/countries/resources/CountryResource.java
@@ -3,6 +3,8 @@ package com.github.eskokado.eskcitiesapi.countries.resources;
 import com.github.eskokado.eskcitiesapi.countries.entities.Country;
 import com.github.eskokado.eskcitiesapi.countries.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,9 @@ public class CountryResource {
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries() {
-        return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+
+        return repository.findAll(page);
     }
 
 }
